1739974431f0cf0fdae042195cc9235dd8974ce3
agchan_san <yuuki617phone@gmail.com>
2023-09-18 19:44:59 +0900

    sum jikkou

--- main.c
+++ main.c
@@ -6,5 +6,11 @@ int main(int args,char** argv){
   printf("Hello world\n");
   int a = hikizan(5,10);
   printf("5 - 10 = %d\n",a);
+  int arr[] = {1,2,3,4,5};
+  const int arr_size = 5;
+
+  int s = sum(arr,arr_size);
+  printf("sum = %d\n",s);
+
   return 0;
 }
