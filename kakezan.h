c2f428fa78aa97dcc3d6e5a840d9fec2a6e8b23b
agchan_san <yuuki617phone@gmail.com>
2023-09-18 20:21:20 +0900

    file tukutta

---
+++ kakezan.h
@@ -0,0 +1,7 @@
+#ifndef KAKEZAN_H
+#define KAKEZAN_H
+
+//returns a * b
+int kakezan(int a,int b);
+
+#endif
---
+++ warizan.h
@@ -0,0 +1,8 @@
+#ifndef WARIZAN_H
+#define WARIZAN_H
+
+//returns a / b 
+//if b==0, return -1
+int warizan(int a,int b);
+
+#endif
