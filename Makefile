f953b7e9a7bf9cef21a08ce0cfb871dab63f45f7
agchan_san <yuuki617phone@gmail.com>
2023-09-18 17:32:24 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+git_tutorial: main.o
+					gcc -o git_tutorial main.o
+
+main.o:   main.c
+					gcc -c main.c
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int args,char** argv){
+  printf("Hello world\n");
+  return 0;
+}
