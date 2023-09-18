c1c1a4dfa70af90c8ae258134f84eb601cc42546
agchan_san <yuuki617phone@gmail.com>
2023-09-18 19:36:56 +0900

    add sum function on .c

--- tasizan.c
+++ tasizan.c
@@ -5,3 +5,12 @@ int tasizan(int a,int b){
   return a + b;
 }
 
+int sum(int* data,int size){
+  if(size<0)return -1;
+  if(size == 0)return 0;
+
+  int ret = 0;
+  for(int i=0;i<size;i++)ret += data[i];
+
+  return ret;
+}
