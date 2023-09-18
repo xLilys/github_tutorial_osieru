9f292007024701a7f308fc7bcab336d044ed6ef6
agchan_san <yuuki617phone@gmail.com>
2023-09-18 18:30:25 +0900

    hikzian dekita

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main(int args,char** argv){
   printf("Hello world\n");
-  int a = tasizan(5,10);
-  printf("5 + 10 = %d\n",a);
+  int a = hikizan(5,10);
+  printf("5 - 10 = %d\n",a);
   return 0;
 }
